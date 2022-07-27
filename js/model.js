78af072f562571696fc6d0208e6b032f76c7810b
pavelzelencow <a2621086@iclod.com>
2022-07-27 22:57:16 +0300

    add: toggle func

--- js/model.js
+++ js/model.js
@@ -2,37 +2,53 @@ class Model {
     constructor() {
         // создаем массив обьектов
         this.cardArray = [{
-            name: '2',
-            img: 'images/card-img/2.png'
-        },
-        {
-            name: 'cc',
-            img: 'images/card-img/cc.png'
-        },
-        {
-            name: 'java',
-            img: 'images/card-img/java.png'
-        },
-        {
-            name: 'phyton',
-            img: 'images/card-img/phyton.png'
-        },
-        {
-            name: 'php',
-            img: 'images/card-img/php.png'
-        },
-        {
-            name: 'sql',
-            img: 'images/card-img/sql.png'
-        },
-        {
-            name: 'sass',
-            img: 'images/card-img/sass.png'
-        },
-        {
-            name: 'react',
-            img: 'images/card-img/react.png'
-        }
+                name: '2',
+                img: 'images/card-img/2.png',
+                imgBack: 'images/card-bg.png',
+                id: 1,
+            },
+            {
+                name: 'cc',
+                img: 'images/card-img/cc.png',
+                imgBack: 'images/card-bg.png',
+                id: 2,
+            },
+            {
+                name: 'java',
+                img: 'images/card-img/java.png',
+                imgBack: 'images/card-bg.png',
+                id: 3,
+            },
+            {
+                name: 'phyton',
+                img: 'images/card-img/phyton.png',
+                imgBack: 'images/card-bg.png',
+                id: 4,
+            },
+            {
+                name: 'php',
+                img: 'images/card-img/php.png',
+                imgBack: 'images/card-bg.png',
+                id: 5,
+            },
+            {
+                name: 'sql',
+                img: 'images/card-img/sql.png',
+                imgBack: 'images/card-bg.png',
+                id: 6,
+            },
+            {
+                name: 'sass',
+                img: 'images/card-img/sass.png',
+                imgBack: 'images/card-bg.png',
+                id: 7,
+            },
+            {
+                name: 'react',
+                img: 'images/card-img/react.png',
+                imgBack: 'images/card-bg.png',
+                id: 8,
+            }
         ];
     }
     sortArray() {
@@ -47,6 +63,26 @@ class Model {
         return allCards.sort(function () {
             return 0.5 - Math.random()
         });
+    }
+
+    toggleCard() {
+        const card = document.querySelectorAll('.card');
+        const cardBack = document.querySelector('.card__back');
+        const cardFront = document.querySelector('.card__front');
+        console.log(card)
+        card.forEach((item) => {
+            item.addEventListener('click', function () {
+                if (cardFront.classList.contains('hidden')) {
+                    cardBack.classList.add('hidden');
+                    cardFront.classList.remove('hidden');
+                } else {
+                    cardBack.classList.remove('hidden');
+                    cardFront.classList.add('hidden');
+                }
+            })
+        })
 
     }
+
+
 }
