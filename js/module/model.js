64e5ae5fe6d900c34ce9e54085d1fb4f4f919182
Listik <shenja722@gmail.com>
2022-08-14 21:00:43 +0300

    popup

--- js/module/model.js
+++ js/module/model.js
@@ -161,4 +161,29 @@ export default class Model {
             time.textContent = `${minutesGame}:${secondsGame}`;
         }, 1000);
     }
+    popUp() {
+        let popupBg = document.querySelector('.popup__bg');
+        let popup = document.querySelector('.popup');
+        let closePopupButton = document.querySelector('.close-popup');
+
+        openPopupButtons.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                e.preventDefault();
+                popupBg.classList.add('active');
+                popup.classList.add('active');
+            })
+        });
+
+        closePopupButton.addEventListener('click', () => {
+            popupBg.classList.remove('active');
+            popup.classList.remove('active');
+        });
+
+        document.addEventListener('click', (e) => {
+            if (e.target === popupBg) {
+                popupBg.classList.remove('active');
+                popup.classList.remove('active');
+            }
+        });
+    }
 }
